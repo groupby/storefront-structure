e313c54021016d979ed9629996b02c89da346833
Haowei-Guo <haowei.guo@groupbyinc.com>
2017-06-29 11:35:41 -0400

    WIP: work on carousel state and props

--- src/carousel/index.ts
+++ src/carousel/index.ts
@@ -2,13 +2,26 @@ import { tag, Tag } from '@storefront/core';
 
 @tag('gb-carousel', require('./index.html'))
 class Carousel {
-
-  refs: {};
+  props: Carousel.Props = {
+    items: [],
+    prevIcon: '',
+    nextIcon: ''
+  };
+  state: Carousel.State = {
+    index: 0
+  };
 }
 
 interface Carousel extends Tag<Carousel.Props> { }
 namespace Carousel {
   export interface Props {
+    items?: any[];
+    prevIcon?: string;
+    nextIcon?: string;
+  }
+
+  export interface State {
+    index: number
   }
 }
 
--- test/unit/carousel.ts
+++ test/unit/carousel.ts
@@ -5,4 +5,16 @@ suite('Carousel', ({ expect }) => {
   let carousel: Carousel;
 
   beforeEach(() => carousel = new Carousel());
+
+  describe('constructor()', () => {
+    it('should set initial values', () => {
+      expect(carousel.props).to.eql({ items: [] });
+    });
+
+    describe('state', () => {
+      it('should set inital index to 0', () => {
+        expect(carousel.state.index).to.eq(0);
+      });
+    });
+  });
 });
