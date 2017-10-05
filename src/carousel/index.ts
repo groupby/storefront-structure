e3499b8be847fb480319cc930f67e5fbbb794423
Mengqi <mengqi.zhu@groupbyinc.com>
2017-10-05 13:49:51 -0400

    add image

--- src/carousel/index.ts
+++ src/carousel/index.ts
@@ -19,28 +19,28 @@ class Carousel {
   maxOffset: number = 0;
   pageSize: number = 0;
 
-//   onMount() {
-//     this.calculateAttributes();
-//   }
+  onMount() {
+    this.calculateAttributes();
+  }
 
-//   onUpdate() {
-//     this.calculateAttributes();
-//   }
+  onUpdate() {
+    this.calculateAttributes();
+  }
 
-//   calculateAttributes() {
-//     const viewportWidth = parseFloat(utils.WINDOW().getComputedStyle(this.refs.viewport).width);
-//     const itemWidth = parseFloat(utils.WINDOW().getComputedStyle(this.refs.listwrapper).width) / this.props.items.length;
-//     this.pageSize = Math.floor(viewportWidth / itemWidth);
-//     this.maxOffset = Math.max(0, Math.floor((this.props.items.length - 1) / this.pageSize)) * this.pageSize;
-//   }
+  calculateAttributes() {
+    const viewportWidth = parseFloat(utils.WINDOW().getComputedStyle(this.refs.viewport).width);
+    const itemWidth = parseFloat(utils.WINDOW().getComputedStyle(this.refs.listwrapper).width) / this.props.items.length;
+    this.pageSize = Math.floor(viewportWidth / itemWidth);
+    this.maxOffset = Math.max(0, Math.floor((this.props.items.length - 1) / this.pageSize)) * this.pageSize;
+  }
 
-//   onClickPrev = () => {
-//     this.set({ offset: Math.max(0, this.state.offset - this.pageSize) });
-//   }
+  onClickPrev = () => {
+    this.set({ offset: Math.max(0, this.state.offset - this.pageSize) });
+  }
 
-//   onClickNext = () => {
-//     this.set({ offset: Math.min(this.state.offset + this.pageSize, this.maxOffset) });
-//   }
+  onClickNext = () => {
+    this.set({ offset: Math.min(this.state.offset + this.pageSize, this.maxOffset) });
+  }
 }
 
 interface Carousel extends Tag<Carousel.Props> { }
