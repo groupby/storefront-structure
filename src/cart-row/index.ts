b0b67001af2ea3eb1146717cb323160a295583b1
Mengqi <mengqi.zhu@groupbyinc.com>
2017-12-14 17:32:41 -0500

    move data management to cart row internally

--- src/cart-row/index.ts
+++ src/cart-row/index.ts
@@ -1,12 +1,22 @@
 import { alias, tag, Events, Selectors, Tag } from '@storefront/core';
 
-// @alias('cartRow')
+@alias('cartRow')
 @tag('gb-cart-row', require('./index.html'), require('./index.css'))
 class CartRow {
-
-  onUpdate() {
-    console.log('hi', this.props.products);
+  constructor() {
+    this.getCartContent();
   }
+
+  init() {
+    // when persist the store on first load, this will fire
+    this.flux.on(Events.CART_ITEMS_UPDATED, this.getCartContent);
+  }
+
+  getCartContent = () => {
+    const cart = this.select(Selectors.cart);
+    this.set({ cartContent: cart.content.items });
+  }
+
 }
 
 interface CartRow extends Tag<CartRow.Props, CartRow.State> { }
