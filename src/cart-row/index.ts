80913044e49d2556e14406b09c3d5679e123cc22
Mengqi <mengqi.zhu@groupbyinc.com>
2017-12-14 09:45:44 -0500

    plug in data to cart row

--- src/cart-row/index.ts
+++ src/cart-row/index.ts
@@ -3,12 +3,16 @@ import { alias, tag, Events, Selectors, Tag } from '@storefront/core';
 // @alias('cartRow')
 @tag('gb-cart-row', require('./index.html'), require('./index.css'))
 class CartRow {
-  
+
+  onUpdate() {
+    console.log('hi', this.props.products);
+  }
 }
 
 interface CartRow extends Tag<CartRow.Props, CartRow.State> { }
 namespace CartRow {
   export interface Props extends Tag.Props {
+    products: any[];
   }
 
   export interface State {
